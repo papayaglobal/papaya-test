1ef04ffd4a999ae72abcce2e34dd15e3a9f7e073
Nitsan Avni <nitsan@papayaglobal.com>
2018-09-06 18:02:23 +0300

    simpler incorrect implementation

--- dependencies/java/src/main/java/Solver.java
+++ dependencies/java/src/main/java/Solver.java
@@ -1,22 +1,7 @@
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 final class Solver {
 
     static String[] solve(String[][] dependencies) {
-        return incorrectlySolve(dependencies);
-    }
-
-    private static String[] incorrectlySolve(String[][] dependencies) {
-        return flatten(dependencies)
-                .distinct()
-                .toArray(String[]::new);
-    }
-
-    private static <T> Stream<T> flatten(T[][] twoDArray) {
-        return Arrays
-                .stream(twoDArray)
-                .flatMap(Arrays::stream);
+        return new String[]{"a", "b", "c"};
     }
 
 }
